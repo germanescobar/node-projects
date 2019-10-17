ab6d91ed206f2631b02edcfbb78cff08fa7bb481
Germán Escobar <german.escobarc@gmail.com>
2019-10-17 09:54:28 -0500

    Implement authentication

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require('cookie-session');
 
 const app = express();
 
@@ -8,6 +9,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/projects', { useNewUrlParser: true, 
 
 //Project Schema
 const projectSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
   name: {
     type: String,
     required: [true, "is required"]
@@ -19,12 +24,41 @@ const projectSchema = mongoose.Schema({
 //ProjectModel
 const Project = mongoose.model("Project", projectSchema);
 
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
 //Middlewares
 app.use(express.static("public"));
 app.use(express.json());
 
+app.use(cookieSession({
+  secret: "my secret",
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+
+const requireUser = (req, res, next) => {
+  if (!req.session.userId) {
+    res.status(401).json({ error: "Not Authenticated" });
+    return;
+  }
+
+  next();
+}
+
 //GET projects
-app.get("/projects", async (req, res, next) => {
+app.get("/projects", requireUser, async (req, res, next) => {
   try {
     const projects = await Project.find();
     res.json(projects);
@@ -34,26 +68,60 @@ app.get("/projects", async (req, res, next) => {
 });
 
 //POST projects
-app.post("/projects", async (req, res, next) => {
-  const newProject = req.body;
+app.post("/projects", requireUser, async (req, res, next) => {
+  const newProject = { name: req.body.name, description: req.body.description };
   newProject.creationDate = new Date();
+  console.log("User Id: ", req.session.userId);
+  newProject.user = req.session.userId;
 
   try {
     const project = await Project.create(newProject)
     res.json(project);
   } catch (err) {
-    if (err.name === "ValidationError") {
-      res.status(422).json({ errors: err.errors });
+    next(err);
+  }
+});
+
+app.post("/register", async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const user = await User.create({ email, password });
+    req.session.userId = user._id;
+    res.json({ ok: true });
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post("/login", async (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  try {
+    const user = await User.find({ email });
+
+    if (user && user.password === password) {
+      req.session.userId = user._id;
+      res.json({ ok: true });
     } else {
-      next(err);
+      res.status(401).json({ error: "Invalid credentials "});
     }
+  } catch (err) {
+    next(err);
   }
 });
 
 // Manejo de errores
 app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ error: err.message });
+  if (err.name === "ValidationError") {
+    res.status(422).json({ errors: err.errors });
+  } else {
+    // error inesperado
+    console.log(err);
+    res.status(500).json({ error: err.message });
+  }
 });
 
 //Initialize Server
